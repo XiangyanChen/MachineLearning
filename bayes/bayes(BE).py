a9848ad4965685783bd969324450723da8b4bee0
xychen <13030880@qq.com>
2019-04-17 11:15:34 +0800

    add bayes based on maxmium likelihood estimation and bayes based on bayes estimation

--- bayes/bayes(BE).py
+++ bayes/bayes(BE).py
@@ -1,11 +1,22 @@
 # coding:utf-8
+# Designed by Chen
+# 2017-8-19
 import numpy as np
+class bayes:
+    def __init__(self, lamb = 1):
+        if (lamb < 0):  #因为lamb大于等于0
+            self._lamb = 1
+        else:
+            self._lamb = lamb
 
-class Bayes:
-    def create_vocab_list(self, dataSet): #创建词汇表 create vocab list, like[1, 2, 3, 'S', 'M', 'L']
-        vocab_set = set()
-        for document in dataSet:
-            vocab_set = vocab_set | set(document)
+    def create_vocab_list(self, dataSet): #创建词汇表
+        vocab_set = set([])
+        m, n = np.shape(dataSet)    #获得数据集的行和列
+        self._S = [] #每个特征在自己的维度里出现的次数
+        for i in range(n):  #按维度来创建词条，第一维，第二维这样
+            column = set([row[i] for row in dataSet])
+            vocab_set = vocab_set | set(column)
+            self._S.extend(list(np.ones(len(column)) * len(column)))
         return list(vocab_set)
 
     def set_of_word2vec(self, vocab_list, input_set):   #词条向量 return feature vector, like a feature vector is [1, 'S']，if vocab_list is [1, 2, 3, 'S', 'M', 'L']，return vocab vector [1, 0, 0, 1, 0, 0]
@@ -15,50 +26,75 @@ class Bayes:
                 vocab_vec[vocab_list.index(word)] = 1    #如果发现有匹配的值就设置为1
         return vocab_vec
 
-    def train(self, dataSet, labels):   #训练样本 train
-        self._vocab_list = self.create_vocab_list(dataSet)  #创建特征词汇表 create vocab list
-        train_matrix = []    #多条词条向量的矩阵（一个词条向量代表着一个样本在词条中出现的次数） matrix consists of vocab vector
-        for line in dataSet:    #将每个训练样本转换为词条向量 feature vector to vocab vector
-            train_matrix.append(self.set_of_word2vec(self._vocab_list, line))
-        n = len(self._vocab_list) #词条的特征数   feature num
-        negative_feature_num = np.zeros(n)    #在类别为-1时，出现特征的次数向量（n1 means negative 1）,the vector of counting num of every feature when label equal -1
-        positve_feature_num = np.zeros(n)    #在类别为1时，出现特征的次数向量（）
-        negative_num = 0    #标签中出现-1的次数 counting the number of negative label
+    def train(self, dataSet, labels):   #训练样本
+        self._vocab_list = self.create_vocab_list(dataSet)  # 创建特征词汇表 create vocab list
+        train_matrix = []  # 多条词条向量的矩阵（一个词条向量代表着一个样本在词条中出现的次数） matrix consists of vocab vector
+        for line in dataSet:  # 将每个训练样本转换为词条向量 feature vector to vocab vector
+            train_matrix.append(self.set_of_word2vec(self.vocab_list, line))
+        n = len(self.vocab_list)  # 词条的特征数   feature num
+        print(n)
+        negative_feature_num = np.zeros(n)  # 在类别为-1时，出现特征的次数向量（n1 means negative 1）,the vector of counting num of every feature when label equal -1
+        positve_feature_num = np.zeros(n)  # 在类别为1时，出现特征的次数向量（）
+        negative_num = 0  # 标签中出现-1的次数 counting the number of negative label
         positive_num = 0
         for i in range(len(train_matrix)):
             if labels[i] == 1:
                 positive_num += 1
                 positve_feature_num += train_matrix[i]
             else:
-                negative_feature_num += train_matrix[i] #与词条向量相加
+                negative_feature_num += train_matrix[i]  # 与词条向量相加
                 negative_num += 1
-        self._positive_vec = positve_feature_num / positive_num   #类别为1的各个随机向量（特征）的概率分布    the probability of feture num
-        self._negative_vec = negative_feature_num / negative_num
-        self._p_positive = positive_num / float(len(labels)) #p(y=1)的概率 the probability of positive label
-        return self._positive_vec, self._negative_vec, self._p_positive
+        self._positive_vec =  (positve_feature_num + self.lamb) / (positive_num + np.array(self.S) * self.lamb)  #p1的各个随机向量（特征）的概率分布
+        self._negative_vec = (negative_feature_num + self.lamb) / (negative_num + np.array(self.S)  * self.lamb)
+        self._p_positive = (positive_num + self.lamb) / float(len(labels) + len(set(labels)) * self.lamb) #p(y=1)的概率
+        # return self.p1Vect, self.pN1Vect, self.pClass1
 
     def predict(self, input_data):   #预测函数
-        input_vec = self.set_of_word2vec(self._vocab_list, input_data)#测试样本的词条向量
+        input_vec = self.set_of_word2vec(self.vocab_list, input_data)  # 测试样本的词条向量
         # np.multiply(self.p1Vect ,inputVec)
-        p_positive = self._p_positive   #按照公式需要乘以p(y=1)的值，我们就以此为初始值
-        p_negative = (1 - self._p_positive)
-        for num in np.multiply(self._positive_vec ,input_vec):  #概率分布和词条向量进行相乘，得出p(x=xi|y=1)的概率，然后相乘
+        p_positive = self.p_positive  # 按照公式需要乘以p(y=1)的值，我们就以此为初始值
+        p_negative = (1 - self.p_positive)
+        for num in np.multiply(self.positive_vec, input_vec):  # 概率分布和词条向量进行相乘，得出p(x=xi|y=1)的概率，然后相乘
             if (num > 0):
                 p_positive *= num
-        for num in np.multiply(self._negative_vec ,input_vec):
+        for num in np.multiply(self.negative_vec, input_vec):
             if (num > 0):
                 p_negative *= num
         print(p_positive, p_negative)
-        if (p_positive > p_negative):  #相比，谁大就倾向谁 up to max probability
+        if (p_positive > p_negative):  # 相比，谁大就倾向谁 up to max probability
             return 1
         else:
             return -1
+
+    @property
+    def lamb(self):
+        return self._lamb
+
+    @property
+    def vocab_list(self):
+        return self._vocab_list
+
+    @property
+    def positive_vec(self):
+        return self._positive_vec
+
+    @property
+    def negative_vec(self):
+        return self._negative_vec
+
+    @property
+    def p_positive(self):
+        return self._p_positive
+
+    @property
+    def S(self):
+        return self._S
 
 if __name__ == "__main__":
     dataSet = [[1, "S"], [1, "M"], [1, "M"], [1, "S"], [1, "S"],
                [2, "S"], [2, "M"], [2, "M"], [2, "L"], [2, "L"],
                [3, "L"], [3, "M"], [3, "M"], [3, "L"], [3, "L"]]
     labels = [-1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1]
-    bayes = Bayes()
+    bayes = bayes()
     bayes.train(dataSet, labels)
     print("prediction is:", bayes.predict([2, "S"]))
--- bayes/bayes(MLE).py
+++ bayes/bayes(MLE).py
@@ -2,69 +2,73 @@
 import numpy as np
 
 class Bayes:
-    def train(self, dataSet, labels):   #训练样本 train
-        self._vocab_list = self.create_vocab_list(dataSet)  #创建特征词汇表 create vocab list
-        train_matrix = []    #多条词条向量的矩阵（一个词条向量代表着一个样本在词条中出现的次数）
-        for line in dataSet:    #将每个训练样本转换为词条向量
-            train_matrix.append(self.set_of_word2vec(self._vocab_list, line))
-        n = len(self._vocab_list) #词条的特征数
-        pn1_num = np.zeros(n)    #在类别为-1时，出现特征的次数向量（n1 means negative 1）
-        p1_num = np.zeros(n)    #
-        num_of_pn1 = 0    #标签中出现-1的次数
-        num_of_p1 = 0
-        for i in range(len(train_matrix)):
-            if labels[i] == 1:
-                p1_num += train_matrix[i] #与词条向量相加
-                print(train_matrix[i])
-                num_of_p1 += 1
-            else:
-                pn1_num += train_matrix[i]
-                num_of_pn1 += 1
-            # print trainMatrix[i]
-        self._p1_vec =  p1_num / num_of_p1   #p1的各个随机向量（特征）的概率分布
-        self._pn1_vec = pn1_num / num_of_pn1
-        self._p_class_1 = num_of_p1 / float(len(labels)) #p(y=1)的概率
-        return self._p1_vec, self._pn1_vec, self._p_class_1
-
-    def predict(self, inputData):   #预测函数
-        inputVec = self.set_of_word2vec(self._vocab_list, inputData)#测试样本的词条向量
-        # np.multiply(self.p1Vect ,inputVec)
-        p1 = self._p_class_1   #按照公式需要乘以p(y=1)的值，我们就以此为初始值
-        p_n1 = (1 - self._p_class_1)
-        for num in np.multiply(self._p1_vec ,inputVec):  #概率分布和词条向量进行相乘，得出p(x=xi|y=1)的概率，然后相乘
-            if (num > 0):
-                p1 *= num
-        for num in np.multiply(self._pn1_vec ,inputVec):
-            if (num > 0):
-                p_n1 *= num
-        print(p1, p_n1)
-        if (p1 > p_n1):  #相比，谁大就倾向谁
-            return 1
-        else:
-            return -1
-        # pOfLabels = {}
-        # numOfLabels = len(labels)
-        # labelSet = list(set(self.labels))
-        # for i in range(len(labelSet)):  #遍历算出标签的概率
-        #     count = 0
-        #     for j in range(numOfLabels):
-        #         if (labelSet[i] == labels[j]):
-        #             count += 1
-        #         pOfLabels[labelSet[i]] = count / float(numOfLabels)
-        # print pOfLabels[-1]
-
     def create_vocab_list(self, dataSet): #创建词汇表 create vocab list, like[1, 2, 3, 'S', 'M', 'L']
         vocab_set = set()
         for document in dataSet:
             vocab_set = vocab_set | set(document)
         return list(vocab_set)
 
-    def set_of_word2vec(self, vocab_list, input_set):   #词汇表向量
-        return_vec = [0] * len(vocab_list)    #vocablist大小的零向量
-        for word in input_set:   #遍历输入样本的每个特征
+    def set_of_word2vec(self, vocab_list, input_set):   #词条向量 return feature vector, like a feature vector is [1, 'S']，if vocab_list is [1, 2, 3, 'S', 'M', 'L']，return vocab vector [1, 0, 0, 1, 0, 0]
+        vocab_vec = [0] * len(vocab_list)    #vocablist大小的零向量 zero vector
+        for word in input_set:   #遍历输入样本的每个特征   iterating every feature
             if word in vocab_list:
-                return_vec[vocab_list.index(word)] = 1    #如果发现有匹配的值就设置为1
-        return return_vec
+                vocab_vec[vocab_list.index(word)] = 1    #如果发现有匹配的值就设置为1
+        return vocab_vec
+
+    def train(self, dataSet, labels):   #训练样本 train
+        self._vocab_list = self.create_vocab_list(dataSet)  #创建特征词汇表 create vocab list
+        train_matrix = []    #多条词条向量的矩阵（一个词条向量代表着一个样本在词条中出现的次数） matrix consists of vocab vector
+        for line in dataSet:    #将每个训练样本转换为词条向量 feature vector to vocab vector
+            train_matrix.append(self.set_of_word2vec(self.vocab_list, line))
+        n = len(self.vocab_list) #词条的特征数   feature num
+        negative_feature_num = np.zeros(n)    #在类别为-1时，出现特征的次数向量（n1 means negative 1）,the vector of counting num of every feature when label equal -1
+        positve_feature_num = np.zeros(n)    #在类别为1时，出现特征的次数向量（）
+        negative_num = 0    #标签中出现-1的次数 counting the number of negative label
+        positive_num = 0
+        for i in range(len(train_matrix)):
+            if labels[i] == 1:
+                positive_num += 1
+                positve_feature_num += train_matrix[i]
+            else:
+                negative_feature_num += train_matrix[i] #与词条向量相加
+                negative_num += 1
+        self._positive_vec = positve_feature_num / positive_num   #类别为1的各个随机向量（特征）的概率分布    the probability of feture num
+        self._negative_vec = negative_feature_num / negative_num
+        self._p_positive = positive_num / float(len(labels)) #p(y=1)的概率 the probability of positive label
+        # return self._positive_vec, self._negative_vec, self._p_positive
+
+    def predict(self, input_data):   #预测函数
+        input_vec = self.set_of_word2vec(self.vocab_list, input_data)#测试样本的词条向量
+        # np.multiply(self.p1Vect ,inputVec)
+        p_positive = self.p_positive   #按照公式需要乘以p(y=1)的值，我们就以此为初始值
+        p_negative = (1 - self.p_positive)
+        for num in np.multiply(self.positive_vec ,input_vec):  #概率分布和词条向量进行相乘，得出p(x=xi|y=1)的概率，然后相乘
+            if (num > 0):
+                p_positive *= num
+        for num in np.multiply(self.negative_vec ,input_vec):
+            if (num > 0):
+                p_negative *= num
+        print(p_positive, p_negative)
+        if (p_positive > p_negative):  #相比，谁大就倾向谁 up to max probability
+            return 1
+        else:
+            return -1
+
+    @property
+    def vocab_list(self):
+        return self._vocab_list
+
+    @property
+    def positive_vec(self):
+        return self._positive_vec
+
+    @property
+    def negative_vec(self):
+        return self._negative_vec
+
+    @property
+    def p_positive(self):
+        return self._p_positive
 
 if __name__ == "__main__":
     dataSet = [[1, "S"], [1, "M"], [1, "M"], [1, "S"], [1, "S"],
@@ -73,5 +77,4 @@ if __name__ == "__main__":
     labels = [-1, -1, 1, 1, -1, -1, -1, 1, 1, 1, 1, 1, 1, 1, -1]
     bayes = Bayes()
     bayes.train(dataSet, labels)
-    print(bayes.predict([2, "S"]))
-    # print bayes.createVocabList(dataSet)
+    print("prediction is:", bayes.predict([2, "S"]))
