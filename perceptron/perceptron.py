ced589d387c253d5fbf006196a1f7082c45b3eb3
xychen <13030880@qq.com>
2019-06-28 21:58:59 +0800

    update perceptron

--- perceptron/perceptron.py
+++ perceptron/perceptron.py
@@ -1,46 +1,50 @@
-# --*-- coding:utf:8 --*--
+"""
+This code is done by FlameCharmander
+which is published in https://github.com/FlameCharmander/MachineLearning
+everyone is welcome to contact me via 13030880@qq.com
+"""
 import numpy as np
 
-class Perceptron:  # 感知机
-    def __init__(self, dataSet, labels):  # 初始化数据集和标签, initial dataset and label
-        self._dataSet = np.array(dataSet)
-        self._labels = np.array(labels).transpose()
+class Perceptron:
+    def __init__(self):
+        self.weights = None
+        self.bias = None
 
-    def train(self):
-        m, n = np.shape(self.dataSet)  # m是行和n是列
-        weights = np.zeros([1, n])  #row vector
+    def sign(self, value): #sign function
+        return 1 if value >= 0 else -1
+
+    def train(self, data_set, labels):  #used to train
+        alpha = 1    #learning rate
+        data_set = np.array(data_set)
+        n = data_set.shape[0]   #n rows
+        m = data_set.shape[1]   #m cols
+        weights = np.zeros(m)   #initialize weight
         bias = 0
-        flag = False
-        while flag != True:
-            flag = True
-            for i in range(m):  # 遍历样本 iterate samples
-                y = weights * np.mat(dataSet[i]).T + bias  # 以向量的形式计算
-                if (self.sign(y) * self.labels[i] < 0):  # 说明是误分类了  it means this is wrong misclassification data
-                    weights += self.labels[i] * self.dataSet[i]  # 更新权重
-                    bias += self.labels[i]  # 更新偏置
-                    print("weights %s, bias %s" % (weights, bias))
-                    flag = False
-        return weights, bias
+        i = 0
+        while i < n:
+            if (labels[i] * self.sign(np.dot(weights, data_set[i]) + bias) == -1):   #it means this point is wrong
+                weights = weights + alpha * labels[i] * data_set[i] #update weights, this is corresponding to (2.6) in P28
+                bias = bias + alpha * labels[i] ##update bias, this is corresponding to (2.6)
+                i = 0
+            else:
+                i += 1
+        self.weights = weights
+        self.bias = bias
 
-    def sign(self, y):  # 符号函数 sign function
-        if (y > 0):
-            return 1
-        else:
-            return -1
-
-    @property
-    def dataSet(self):
-        return self._dataSet
-
-    @property
-    def labels(self):
-        return self._labels
+    def predict(self, data):    #used to predict
+        return self.sign(np.dot(self.weights, data) + self.bias)
 
 if __name__ == "__main__":
-    dataSet = [[3, 3],
+    """
+        this code is corresponding to algorithm(2.1) in P29
+    """
+    data_set = [[3, 3],
                [4, 3],
                [1, 1]]
     labels = [1, 1, -1]
-    perceptron = Perceptron(dataSet, labels)  # 创建一个感知机对象
-    weights, bias = perceptron.train()  # 训练
-    print("final weights:%s, bias:%s" % (weights, bias))
+    perceptron = Perceptron()
+    perceptron.train(data_set, labels)
+    print(perceptron.weights)
+    print(perceptron.bias)
+    result = perceptron.predict([3, 3])
+    print(result)
